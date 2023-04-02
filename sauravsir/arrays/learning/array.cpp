21e5b57d17762c30c3e5a9210bb5a6cb27e9c5cc
Hasan Ali <kk631352@gmail.com>
2023-04-02 17:04:06 +0530

    Added array class and few methods

--- sauravsir/arrays/learning/array.cpp
+++ sauravsir/arrays/learning/array.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 using namespace std;
 
+class Array
+{
+private:
+    int capacity;
+    int *ptr;
+    int lastIndex;
+
+public:
+    Array(int);
+    bool isEmpty();
+    bool isFull();
+    void traverse();
+};
+
+Array::Array(int cap)
+{
+    ptr = new int[cap];
+    lastIndex = -1;
+    capacity = cap;
+}
+
+bool Array::isEmpty()
+{
+    return lastIndex == -1;
+}
+
+bool Array::isFull()
+{
+    return lastIndex == capacity - 1;
+}
+
+void Array::traverse()
+{
+    for (int i = 0; i <= lastIndex; i++)
+        cout << ptr[i] << " ";
+    cout << "\n";
+}
+
 int main()
 {
-    cout << "This is array file\n";
+    Array a1(2);
+    a1.traverse();
+    cout << a1.isEmpty();
+    cout << a1.isFull();
 }
