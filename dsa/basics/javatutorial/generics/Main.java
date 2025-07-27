797b5a2d8a98f0a206cf63d1ea782a3370701ba2
Hasan Ali <hans1998ali@gmail.com>
2025-07-27 13:52:07 +0530

    Commented

--- dsa/basics/javatutorial/generics/Main.java
+++ dsa/basics/javatutorial/generics/Main.java
@@ -4,11 +4,11 @@ public class Main {
    public static void main(String [] args) {
     var genericList = new GenericList<Integer>();
     genericList.add(1);
-    int result = genericList.get(0);
+    // int result = genericList.get(0);
     System.out.println(genericList.get(0));
      // genericList.add(1); // This will cause a compile-time error
     
-     var numbers = new GenericClassWithConstraint<Integer>();
+    //  var numbers = new GenericClassWithConstraint<Integer>();
      
      // var error = new GenericClassWithConstraint<String>() compile time error
      System.err.println("Max is " +GenericMethods.maxGeneric(1, 2));
