47ef16ffd888d0a9772817a613398272a211e5f3
Hasan Ali <hans1998ali@gmail.com>
2025-07-27 13:49:40 +0530

    Added array starter

---
+++ striver/basics/arrays/problems/easy/pythoncode/main.py
@@ -0,0 +1,16 @@
+class EasyArrayProblems():
+    def test(self):
+        print("Hello Array Test")
+
+
+
+
+
+def test_easy_array_problems():
+    p = EasyArrayProblems()
+    p.test()
+
+
+
+if __name__ == '__main__':
+    test_easy_array_problems()
